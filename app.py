69727ccdd63acf3d95f34616f21f6762ed349a34
CS783838 <sasha99228833@gmail.com>
2025-04-15 13:34:08 +0200

    first layout

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import streamlit as st
+import yfinance as yf
 
-picked_stock = st.text_input("Please input the ticker of the stock you would like to display")
+st.title("📈 Stock Quarterly Results Tracker")
 
-print("ntm")
+ticker_input = st.text_input("Enter a stock ticker (e.g., AAPL, MSFT):", value="AAPL")
 
+if st.button("Get Earnings Data"):
+    ticker = yf.Ticker(ticker_input.upper())
+    earnings = ticker.quarterly_earnings
 
+    if earnings.empty:
+        st.warning("No quarterly earnings data found.")
+    else:
+        st.subheader("Quarterly Earnings")
+        st.dataframe(earnings)
+
+        
+        st.subheader("Earnings Over Time")
+        fig, ax = plt.subplots()
+        earnings['Earnings'].plot(kind='bar', ax=ax)
+        st.pyplot(fig)
