7eb2204e59ae69707b9fab564f61a78096b72a2a
NilsJ11 <nils.juergensen.student@gmail.com>
2025-05-05 16:33:19 +0200

    stop

--- app.py
+++ app.py
@@ -29,5 +29,3 @@ if st.session_state.get("ticker"):
     st.info(f"Currently analyzing: **{st.session_state['ticker']}**")
 else:
     st.warning("Please enter a valid stock ticker to get started.")
-
-aabb
